baecab721ea52b41b7b874e588758bab2ae028dd
shree-92 <shreessb92@gmail.com>
2024-09-28 23:13:40 +0530

    connected to mongo atlas

---
+++ src/db/index.js
@@ -0,0 +1,17 @@
+import mongoose, { mongo } from "mongoose";
+import {DB_NAME} from "../utils/constants.js"
+
+const connectionStr = process.env.MONGODB_URI
+
+const dbConnect = async () => {
+    try {
+        const connectionResponse  = await mongoose.connect(`${connectionStr}/${DB_NAME}`);
+        console.log(`connection to mongoDB was successful and the host is ${connectionResponse.connection.host} and the entier response is ${connectionResponse}`);
+        
+    } catch (error) {
+        console.log(error, "connection to mongoDB failed");
+        process.exit(1)
+    }
+}
+
+export default dbConnect;
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,1 +1,38 @@
- 
+import dotenv from "dotenv";
+
+import dbConnect from "../db/index.js";
+import e from "express";
+
+const port = process.env.PORT
+const app = e()
+
+dotenv.config({
+    path: './env'
+})
+
+dbConnect()
+
+
+
+
+
+
+
+
+
+
+
+
+
+// ;( async ()=>{
+//     try {
+//         await mongoose.connect(`${connectionStr}/${DB_NAME}`);
+//         app.on("error", (error)=> console.log(error)
+//         )
+//         app.listen(port, ()=> console.log(`app is listenting on ${port}`)
+//         )
+//     } catch (error) {
+//         console.log(error);
+//         throw error;
+//     }
+// } )()
