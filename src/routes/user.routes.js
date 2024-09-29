23dec48bcb48600318a97c49f8021aa61118d197
shree-92 <shreessb92@gmail.com>
2024-09-29 22:00:23 +0530

    this is the base file structure now

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { registerUser } from "../controllers/user.controller.js"; // Check this path
+import { registerUser } from "../controllers/user.controller.js";
 
 const router = Router();
 
